4ce30a9db0a7bac3279514000e4f0d85829d4b84
André Felipe <andre.felipe.2048@gmail.com>
2023-07-26 20:26:01 -0300

    feat: create new entity class Card

---
+++ src/main/java/io/github/andrefelipeos/santanderdevweek2023/domain/model/Card.java
@@ -0,0 +1,32 @@
+package io.github.andrefelipeos.santanderdevweek2023.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Card")
+@Table(name = "tb_card")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(scale = 13, precision = 2)
+    private String limit;
+
+}
