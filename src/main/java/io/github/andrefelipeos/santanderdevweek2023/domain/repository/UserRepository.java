6cf37298ea0f4d3939ff7d1b43d65f9e47f92b0d
André Felipe <andre.felipe.2048@gmail.com>
2023-07-27 16:47:32 -0300

    feat: verify if a given account number is already in use

--- src/main/java/io/github/andrefelipeos/santanderdevweek2023/domain/repository/UserRepository.java
+++ src/main/java/io/github/andrefelipeos/santanderdevweek2023/domain/repository/UserRepository.java
@@ -6,4 +6,6 @@ import io.github.andrefelipeos.santanderdevweek2023.domain.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByAccountNumber(String accountNumber);
+
 }
