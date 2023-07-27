183338ca3833284ed0f514d08006b6d693c2db49
André Felipe <andre.felipe.2048@gmail.com>
2023-07-27 15:38:04 -0300

    feat: create new repository interface UserRepository

---
+++ src/main/java/io/github/andrefelipeos/santanderdevweek2023/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package io.github.andrefelipeos.santanderdevweek2023.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.andrefelipeos.santanderdevweek2023.domain.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
